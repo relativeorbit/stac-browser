e1489931356da567bd0cd518d7365fce389316e7
Scott Henderson <scottyh@uw.edu>
2024-02-19 18:37:54 -0800

    default javascript arg val

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     catalogUrl: null,
-    catalogTitle: "STAC Browser",
+    catalogTitle: "RelativeOrbit STAC Browser",
     allowExternalAccess: true, // Must be true if catalogUrl is not given
     allowedDomains: [],
     detectLocaleFromBrowser: true,
@@ -28,8 +28,8 @@ module.exports = {
         key, // the key or name in the assets object that points to the particular asset
         item, // the STAC item / feature
         bounds, // LatLngBounds of the STAC asset
-        isCOG: true, // true if the asset is definitely a cloud-optimized GeoTIFF
-        isVisual: true, // true when the asset's key is "visual" (case-insensitive)
+        isCOG = true, // true if the asset is definitely a cloud-optimized GeoTIFF
+        isVisual = true, // true when the asset's key is "visual" (case-insensitive)
       }) => {
         // assets has three bands of RGB, so no need to specify bands
         if (isVisual) return "https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url={url}";
@@ -38,7 +38,7 @@ module.exports = {
         // where we might want to ignore the Near-Infrared Band
         else return "https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url={url}&bands=1,2,3"
       }
-    },
+      },
     stacProxyUrl: null,
     pathPrefix: "/",
     historyMode: "history",
